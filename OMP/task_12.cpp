6756d24c95aecda4a93f064beb71753fb087eb49
anpudova <90646684+anpudova@users.noreply.github.com>
2023-09-26 21:52:31 +0300

    Update task_12.cpp

--- OMP/task_12.cpp
+++ OMP/task_12.cpp
@@ -4,14 +4,14 @@
 
 int main()
 {
-	#pragma omp parallel num_threads(8)
-	{
-		for (int i = 7; i >= 0; i--) {
+    #pragma omp parallel num_threads(8)
+    {
+        for (int i = 7; i >= 0; i--) {
 
-			#pragma omp barrier
-			if (i == omp_get_thread_num()) {
-				printf("Thread ID: %d of %d threads. Hello World! \n", omp_get_thread_num(), omp_get_num_threads());
-			}
-		}
+	    #pragma omp barrier
+	    if (i == omp_get_thread_num()) {
+	        printf("Thread ID: %d of %d threads. Hello World! \n", omp_get_thread_num(), omp_get_num_threads());
+	    }
 	}
+    }
 }
